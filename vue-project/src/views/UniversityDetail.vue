bf7d6570b97d71b6ecbb11bc06c0c845ea97e108
DELL <431149990@qq.com>
2026-03-12 11:36:16 +0800

    init

--- vue-project/src/views/UniversityDetail.vue
+++ vue-project/src/views/UniversityDetail.vue
@@ -9,7 +9,7 @@
           <Star :class="isFavorited ? 'fill-current text-yellow-500' : 'text-text-secondary'" class="h-4 w-4" /> {{ isFavorited ? '已收藏' : '收藏' }}
         </button>
         <button type="button" class="btn-outline-secondary flex items-center gap-2" @click="goToCorrectionPage">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
           </svg> 数据纠错
         </button>
